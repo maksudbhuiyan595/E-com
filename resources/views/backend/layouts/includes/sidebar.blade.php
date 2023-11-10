582b5c31eb3d754bffb71d7d6ffb7905a8549a4f
Maksud Bhuiyan <maksudbhuiyan595>
2023-11-10 23:47:35 +0600

    templating for E-com

---
+++ resources/views/backend/layouts/includes/sidebar.blade.php
@@ -0,0 +1,64 @@
+<ul class="sidebar-nav" id="sidebar-nav">
+
+<li class="nav-item">
+  <a class="nav-link " href="index.html">
+    <i class="bi bi-grid"></i>
+    <span>Dashboard</span>
+  </a>
+</li><!-- End Dashboard Nav -->
+
+<li class="nav-item">
+  <a class="nav-link collapsed"  href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Categoires</span>
+  </a>
+ 
+</li><!-- Categories-->
+<li class="nav-item">
+  <a class="nav-link collapsed"  href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Sub Category</span>
+    </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link collapsed"  href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Brands</span>
+    </a>
+</li>
+<!-- Brands -->
+<li class="nav-item">
+  <a class="nav-link collapsed"  href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Products</span>
+    </a>
+</li>
+<!-- products -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Shipping</span>
+    </a>
+</li>
+<!-- shipping -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Orders</span>
+    </a>
+</li>
+<!-- orders -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Roles</span>
+    </a>
+</li>
+<!-- roles -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Users</span>
+    </a>
+</li>
+<!-- users -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#">
+    <i class="bi bi-menu-button-wide"></i><span>Settings</span>
+    </a>
+</li>
+<!-- Seetings -->
+
+</ul>
---
+++ resources/views/backend/master.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  
+  @include('backend.layouts.includes.head')
+ 
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  @include('backend.layouts.includes.navbar')
+  <!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+   @include('backend.layouts.includes.sidebar')
+
+  </aside><!-- End Sidebar-->
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Dashboard</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    @yield('content')
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  @include('backend.layouts.includes.footer')
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  @include('backend.layouts.includes.script')
+</body>
+
+</html>
